fe2b223cf4046ac1725e1ebaccc1a62f49f727d0
github username <github email address>
2020-05-20 13:58:43 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    namespace :v1 do 
+      resources :reviews
+
+      resources :customers do 
+        resources :reviews 
+      end
+      resources :restaurants do 
+        resources :reviews
+      end
+    end
+  end
+
+
 end
